20c87a3b587c1eb79de9805c8b51319802955f02
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-12-15 23:12:24 +0100

    ajout check catégorie

--- src/script.js
+++ src/script.js
@@ -1,17 +1,33 @@
 const fs = require("fs");
 const { connection } = require("./db_connection");
+const categories = require("./categories.json");
 
 const createAllClub = new Promise((resolve, reject) => {
   console.log("début");
   let clubs = [];
+  let cats = [];
   const sql = "SELECT * FROM `TABLE 2` ";
   connection.query(sql, (err, results) => {
+    console.log(results[0]);
     if (err) {
       console.log(err);
       reject();
     } else {
       let clubsID = [];
+
       results.forEach((element) => {
+        // Modification des catégorie
+        results.forEach((element) => {
+          if (element["Libellé catégorie"] === "Foot Loisir / Foot Loisir") {
+            element["Libellé catégorie"] = "Football Loisir / Football Loisir";
+          }
+        });
+
+        // on controle le nombre de catégorie stocké dans la liste des teams
+        if (!cats.includes(element["Libellé catégorie"])) {
+          cats.push(element["Libellé catégorie"]);
+        }
+
         if (!clubsID.includes(element["Numéro de club"])) {
           clubsID.push(element["Numéro de club"]);
 
@@ -21,15 +37,33 @@ const createAllClub = new Promise((resolve, reject) => {
 
           let teamsNewFormat = [];
           let cat = [];
+          let minAge = 0;
+          let maxAge = 0;
 
           teams.forEach((team) => {
             let newTeam = {
               teamName: team["Nom équipe"],
               categorie: team["Libellé catégorie"],
             };
-            !cat.includes(team["Libellé catégorie"])
-              ? cat.push(team["Libellé catégorie"])
-              : null;
+
+            if (!cat.includes(team["Libellé catégorie"])) {
+              cat.push(team["Libellé catégorie"]);
+
+              let index = categories.findIndex(
+                (categorie) => team["Libellé catégorie"] === categorie.name
+              );
+              if (index === -1) {
+                console.log("error: " + team["Libellé catégorie"]);
+              } else {
+                minAge > categories[index].minAge
+                  ? (minAge = categories[index].minAge)
+                  : null;
+                maxAge < categories[index].maxAge
+                  ? (maxAge = categories[index].maxAge)
+                  : null;
+              }
+            }
+
             teamsNewFormat.push(newTeam);
           });
 
@@ -43,20 +77,24 @@ const createAllClub = new Promise((resolve, reject) => {
             Latitude: element.Lat,
             Longitude: element.Long,
             categories: cat,
+            minAgeInClub: minAge,
+            maxAgeInClub: maxAge,
           };
+
+          // !allCat.includes(element['Libellé catégorie']) ? allCat.push(element['Libellé catégorie']):null
 
           clubs.push(clubsToPush);
         }
       });
 
-      resolve(clubs);
+      resolve({ clubs: clubs, cats: cats });
     }
   });
 });
 
 createAllClub.then((value) => {
-  let data = JSON.stringify(value);
+  console.log(value.clubs[0]);
+  let data = JSON.stringify(value.clubs);
   fs.writeFileSync("data.json", data);
-
-  process.exit(1);
+  // process.exit(1);
 });
