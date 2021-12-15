e2b94055ad3904fc3b0b75728e2dc2f12ba96db5
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-12-15 20:45:32 +0100

    suppression code commenté

--- src/script.js
+++ src/script.js
@@ -1,15 +1,3 @@
-// require('dotenv').config();
-// const PORT = process.env.PORT;
-// const app = require('./app');
-
-// app.listen(PORT, (err) => {
-//   if (err) {
-//     console.error(`Error: ${err.message}`);
-//   } else {
-//     console.log(`Server is running on port: ${PORT}`);
-//   }
-// });
-
 const fs = require("fs");
 const { connection } = require("./db_connection");
 
