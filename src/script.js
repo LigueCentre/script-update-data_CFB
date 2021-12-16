e3abcfad5c60a8b08a2acbbc1c96a405523db94d
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-12-16 11:44:31 +0100

    ajout

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const { connection } = require("./db_connection");
 const categories = require("./categories.json");
+const { default: axios } = require("axios");
 
 const createAllClub = new Promise((resolve, reject) => {
   console.log("début");
@@ -14,8 +15,7 @@ const createAllClub = new Promise((resolve, reject) => {
       reject();
     } else {
       let clubsID = [];
-
-      results.forEach((element) => {
+      results.slice(3000, 3100).forEach((element) => {
         // Modification des catégorie
         results.forEach((element) => {
           if (element["Libellé catégorie"] === "Foot Loisir / Foot Loisir") {
@@ -79,31 +79,22 @@ const createAllClub = new Promise((resolve, reject) => {
             teamsNewFormat.push(newTeam);
           });
 
-          let clubsToPush = {
-            NumClub: element["Numéro de club"],
-            AdressePostale: element["AdressePostale"],
-            Mail: element["MailClub"],
-            Localite: element["Bureau distributeur"],
-            equipes: teamsNewFormat,
-            NomClub: element["Nom de club"],
-            Latitude: element.Lat,
-            Longitude: element.Long,
-            categories: cat,
-            gender: gender,
-            minAgeInClub: minAge,
-            maxAgeInClub: maxAge,
-          };
-          clubs.push(clubsToPush);
+          // on modifie la , dans le nombre pour le traduire en float
+          console.log(element.Lat);
+          const Lat = element.Lat.replace(",", ".");
+          const Long = element.Long.replace(",", ".");
+          console.log(element.Lat.length);
         }
       });
 
-      resolve({ clubs: clubs, cats: cats });
+      resolve({ clubs: clubs, cats: cats, result: results[0] });
     }
   });
 });
 
 createAllClub.then((value) => {
-  console.log(value.clubs[0]);
+  // console.log(value.result);
+  // console.log(value.clubs[0]);
   let data = JSON.stringify(value.clubs);
   fs.writeFileSync("data.json", data);
   process.exit(1);
